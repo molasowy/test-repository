41a1b9eeafda19ea8209541d2f81961fe8206dbf
Mateusz Maszkiewicz <mateusz.maszkiewicz@allegro.com>
2026-02-22 14:19:41 +0100

    Test

--- src/main/java/com/example/workshops/ExampleController.java
+++ src/main/java/com/example/workshops/ExampleController.java
@@ -8,7 +8,11 @@ public class ExampleController {
 
     @GetMapping("/hello")
     public String hello() {
+<<<<<<< Updated upstream
         return "Hello world 3!";
+=======
+        return "Hello world 4!";
+>>>>>>> Stashed changes
     }
 
 }
